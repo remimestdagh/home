bb528d1ccc5150c8698a895dfc61c3f60e7216c2
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-04-27 14:24:48 +0200

    Updates

---
+++ precache-manifest.c5e8e51c3fab0b44787c90db14cb7bb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220f607712211f87bb0ddb78b4d92587",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "31065719170bfb6fd14c",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a8147352b62d9df04879",
+    "url": "/home/static/css/main.584bac79.chunk.css"
+  },
+  {
+    "revision": "31065719170bfb6fd14c",
+    "url": "/home/static/js/2.73412ab1.chunk.js"
+  },
+  {
+    "revision": "76fd5ab146b0d8fb7b899b4b882f747e",
+    "url": "/home/static/js/2.73412ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8147352b62d9df04879",
+    "url": "/home/static/js/main.1209884c.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "08ad9b7ed64af0ed8bf18764ceb83bf0",
+    "url": "/home/static/media/resume.08ad9b7e.pdf"
+  }
+]);
