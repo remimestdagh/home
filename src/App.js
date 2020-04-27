d0bd586fb99a68ab4a6e2ca30f848ac568173c1f
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-04-27 19:01:41 +0200

    biniliefsn

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import MainBody from "./components/MainBody";
 import AboutMe from "./components/AboutMe";
 import Project from "./components/Project";
 import Footer from "./components/Footer";
+import Skills from "./components/Skills"
 
 const App = () =>  (
   <Fragment>
     <MainBody />
     <AboutMe />
+    <Skills />
     <Project />
     <Footer />
   </Fragment>
